9573fa7607525f4a72014e8809519f4219c70174
xiechenglin <1303380546@qq.com>
2020-08-25 18:00:31 +0800

    hadoop初始化

---
+++ src/test/java/cn/linc/hadoop/HadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.linc.hadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
